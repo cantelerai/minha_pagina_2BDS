1341982ddee68e36afcb22773ee76688fc7b32c6
cantelerai <vieira.raiany@escola.pr.gov.br>
2025-05-30 09:30:26 -0300

    tt

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-function verificarParOuImpar() {
-    const num1 = parseInt(document.getElementById("num1").value);
-    const num2 = parseInt(document.getElementById("num2").value);
+let contador = 0;
+const contadorElemento = document.getElementById('contador');
+const botao = document.getElementById('botaoClique');
 
-    if (isNaN(num1) || isNaN(num2)) {
-        document.getElementById("resultado").innerText = "Por favor, insira dois números válidos.";
-        document.getElementById("resultado").classList.remove("par", "impar");
-        return;
-    }
-
-    const soma = num1 + num2;
-    let resultado = (soma % 2 === 0) ? "A soma é Par!" : "A soma é Ímpar!";
-    
-    const resultadoDiv = document.getElementById("resultado");
-    resultadoDiv.innerText = resultado;
-
-    // Altera a classe para o estilo adequado (par ou impar)
-    if (soma % 2 === 0) {
-        resultadoDiv.classList.add("par");
-        resultadoDiv.classList.remove("impar");
-    } else {
-        resultadoDiv.classList.add("impar");
-        resultadoDiv.classList.remove("par");
-    }
-}
+botao.addEventListener('click', () => {
+  contador++;
+  contadorElemento.textContent = contador;
+});
